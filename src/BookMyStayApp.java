55f5d646ab771e7f011e0b242ee580c874b096c4
SIDHARTH <ss0834@srmist.edu.in>
2026-03-10 10:39:58 +0530

    Implement room initialization and details display
    Added UseCase2RoomInitialization class for room details display.

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,29 @@
-public class UseCase1HotelBookingApp{
-public static void main(String[] args){
-System.out.println("welcome to book my stay");
-System.out.println("hotel booking system v1.0");
+public class UseCase2RoomInitialization {
+public static void main(String[] args) {
+Room singleRoom=new SingleRoom();
+Room doubleRoom=new DoubleRoom();
+Room suiteRoom=new SuiteRoom();
+
+int singleAvailability=5;
+int doubleAvailability=3;
+int suiteAvailability=2;
+
+System.out.println("Hotel Room Initialization");
+System.out.println();
+
+System.out.println("Single Room:");
+singleRoom.displayRoomDetails();
+System.out.println("Available: "+singleAvailability);
+System.out.println();
+
+System.out.println("Double Room:");
+doubleRoom.displayRoomDetails();
+System.out.println("Available: "+doubleAvailability);
+System.out.println();
+
+System.out.println("Suite Room:");
+suiteRoom.displayRoomDetails();
+System.out.println("Available: "+suiteAvailability);
 }
 }
+
